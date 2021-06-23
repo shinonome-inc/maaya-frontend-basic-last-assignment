3c6ed9310527416b1ab2297c91928e2b6917f60e
Maaya Wata <ma.watanabe@4nonome.com>
2021-06-23 11:52:52 +0900

    11:52

--- src/stories/atoms-button.stories.js
+++ src/stories/atoms-button.stories.js
@@ -1,6 +1,6 @@
 import Item_button from "../components/atoms/button/button.html";
 import "../components/atoms/button/button.scss";
 
-export default { atbutton: "Atoms/button" }
+export default { atbutton: "Atoms/button" };
 
 export const Example = () => Item_button;
