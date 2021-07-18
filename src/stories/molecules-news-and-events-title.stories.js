b36d94bda79e9f1cb11557d540460366c4c9add1
Maaya Wata <ma.watanabe@4nonome.com>
2021-07-18 21:56:26 +0900

    全体作成

--- src/stories/molecules-news-and-events-title.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_news from "../components/molecules/news-and-events-title/news-and-events-title.html";
-import "../components/molecules/news-and-events-title/news-and-events-title.scss";
-
-export default { title: "Molecules/Newsandeventstitle" };
-
-export const Default = () => Item_news;
