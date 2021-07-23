1a08b95819d17651cbf2437cdd4e7748ad47f09b
Maaya Wata <ma.watanabe@4nonome.com>
2021-07-24 03:06:34 +0900

    html の作成

--- src/stories/molecules-news-and-events-more.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_more from "../components/molecules/news-and-events-more/news-and-events-more.html";
-import "../components/molecules/news-and-events-more/news-and-events-more.scss";
-
-export default { title: "Molecules/Newsandeventsmore" };
-
-export const Default = () => Item_more;
