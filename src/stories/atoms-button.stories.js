a53529ac8011f8660a4a05d64bb4cd5ca3f9da02
Maaya Wata <ma.watanabe@4nonome.com>
2021-07-24 21:31:18 +0900

    stories.js追加

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Item_button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Button" }
+
+export const Example = () => Item_button;
