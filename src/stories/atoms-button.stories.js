022c135194e72fbe0e1e91c0ee569ac2407b0c5d
ワタナベマアヤ <85225451+maayawatanabe@users.noreply.github.com>
2021-07-24 21:43:45 +0900

    Delete src/stories directory

--- src/stories/atoms-button.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_button from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "Atoms/Button" }
-
-export const Example = () => Item_button;
