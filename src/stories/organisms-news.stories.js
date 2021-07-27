5867560568472ee30a0740b40b9cdde9941b852d
Maaya Wata <ma.watanabe@4nonome.com>
2021-07-28 00:12:25 +0900

    html.css.jsの作成

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Item_ornews from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "Organisms/News" }
+
+export const Example = () => Item_ornews;
