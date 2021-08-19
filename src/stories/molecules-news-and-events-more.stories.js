c46521c44892c42376e60bddb2a766cbee3dbc46
Maaya Wata <ma.watanabe@4nonome.com>
2021-08-19 17:24:47 +0900

    moleculesstoriesの追追加

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Item_more from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/Newsandeventsmore" };
+
+export const Default = () => Item_more;
